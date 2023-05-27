c44a61f57bca186c1cbf8a632a47387dbbb927b1
Gwonil Shin <babo5774@naver.com>
2023-05-27 19:15:10 +0900

    feat: section_by_time 추가

--- config/urls.py
+++ config/urls.py
@@ -10,7 +10,7 @@ from user import views as member_views
 from disease import views as disease_views
 from disease_by_section import views as plants_by_disease_views
 from farm import views as plants_group_views
-
+from section_by_time import views as section_by_time_views
 
 router = routers.DefaultRouter()
 router.register(r'user', member_views.MemberListAPI)
@@ -18,6 +18,7 @@ router.register(r'plants_section', board_views.BoardListAPI)
 router.register(r'disease', disease_views.DiseaseListAPI)
 router.register(r'disease_by_section', plants_by_disease_views.PlantsByDiseaseListAPI)
 router.register(r'farm', plants_group_views.PlantsGroupListAPI)
+router.register(r'seciton_by_time', section_by_time_views.SectionByTimeListAPI)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
