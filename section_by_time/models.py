b9b4c2a31296d4001ec644b3837bd55061dac043
Gwonil Shin <babo5774@naver.com>
2023-05-28 14:14:42 +0900

    feat: section_by_time 테이블 추가
    -section의 시간대별 상태를 저장하는 테이블 추가

--- section_by_time/models.py
+++ section_by_time/models.py
@@ -3,8 +3,9 @@ from plants_section.models import PlantsSection
 from datetime import datetime
 
 def user_directory_path(instance,filename):
-    name='input_image.jpg'
-    return 'image/{0}/{1}/{2}/{3}'.format(instance.user.id,instance.plant_group.name,instance.plant_group.board_cnt,name)
+    name=str(datetime.now().strftime("%Y-%m-%d-%H"))+'h_input_image.jpg'
+    return 'image/{0}/{1}/{2}/{3}'.format(
+        instance.section.farm.user.id,instance.section.farm.name,instance.section.name,name)
 
 class SectionByTime(models.Model):
     id=models.AutoField(primary_key=True)
@@ -12,7 +13,6 @@ class SectionByTime(models.Model):
     image=models.ImageField(upload_to=user_directory_path,null=True)
     status=models.CharField(max_length=1, default='0')
     section=models.ForeignKey(PlantsSection,on_delete=models.CASCADE,null=True)
-
 
     class Meta:
         managed = True
